ec35f9b26b2e9db9e609eb5f509415ab524b8609
ude <uperpetual3@gmail.com>
2022-12-03 10:16:25 +0000

    task 0

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,4 +1,4 @@
-#ifndef LISTS_H
+fndef LISTS_H
 
 #define LISTS_H
 
@@ -8,21 +8,7 @@
 
 
 
-/**
 
- * struct listint_s - singly linked list
-
- * @n: integer
-
- * @next: points to the next node
-
- *
-
- * Description: singly linked list node structure
-
- * for Holberton project
-
- */
 
 typedef struct listint_s
 
